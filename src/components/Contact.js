312a53540ada7c48cd82941a8a47010497166925
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2023-03-20 13:49:53 -0400

    added some images to contacts tab as well as a second project to projects tab

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./Contact.css"
+import gitImg from "./images/github-small.png"
+import linkedinImg from "./images/linkedin-small.svg"
 
 
 const Contact = () => {
@@ -9,8 +11,8 @@ const Contact = () => {
         <div className="contact-container">
             <h3>Contact Me</h3>
             <ul>
-                <li><a href='https://github.com/NyqEvo' target="_blank">GitHub</a></li>
-                <li><a href='https://www.linkedin.com/in/xavier-vergara/'>LinkedIn</a></li>
+                <li><a href='https://github.com/NyqEvo' target="_blank"><img src={gitImg}/>GitHub</a></li>
+                <li><a href='https://www.linkedin.com/in/xavier-vergara/'><img src={linkedinImg}/>LinkedIn</a></li>
                 <li id="email" onClick={() => window.location = 'mailto:xav.vergara13@gmail.com'}>xav.vergara13@gmail.com</li>
             </ul>
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './Projects.css'
-import gif from './images/task-gif.gif';
+import './Projects.css';
+import taskGif from './images/task-gif.gif';
+import thoughtsGif from './images/social-network-thoughts.gif';
 
 
 const Projects = () => {
@@ -11,9 +12,12 @@ const Projects = () => {
             <p>This section will display all my relevant projects. "Task Sandwich will be the first one shown, with other projects such as mongoDB, mongoose, and back-end routing. Gifs or Pictures will be shown with short descriptions but I'm not sure what is most professional atm.</p>
 
             <h2>Task Sandwich</h2>
-            <img src={gif} alt='gif demonstrating the Task Sandwich site'/>
+            <img src={taskGif} alt='gif demonstrating the Task Sandwich site'/>
             <p>Task Sandwich is a project managment site with a reward system included. Users can earn reward credits to spend however they agree on spending them for their own real life rewards. Using Apollo GraphQL and MongoDb, we were able to successfully store data (with encryption for passwords) and send data to front end with a viariety of typeDefs and resolvers.</p>
 
+            <h2>"Social Network" Back-End</h2>
+            <img src={thoughtsGif} alt='gif demonstrating the social network thoughts project'/>
+            <p>This is a project done to demonstrate my ability to both create a functioning database as well as retrieve, update, add, and delete information in the database from the front end.</p>
         </div>
     )
 }
