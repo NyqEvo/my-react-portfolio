ecb664fdae2978214f5672624c74a322eae3c5cb
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2023-03-21 10:46:03 -0400

    added new resume picture and download link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,7 +6,7 @@ function Resume() {
     return (
         <div className='resume-container'>
             <img src={resume} alt='picture of my resume' />
-            <a href='https://drive.google.com/file/d/1qyl8QqzuwrfDc6K_dY0h-RxETW3Kt6sJ/view?pli=1' target='_blank'><p>Click here to download my resume</p></a>
+            <a href='https://drive.google.com/file/d/1WckZekqr-0QOJRc2sdnagoAk_x2NhJN2/view?usp=sharing' target='_blank'><p>Click here to download my resume</p></a>
         </div>
     )
 }
